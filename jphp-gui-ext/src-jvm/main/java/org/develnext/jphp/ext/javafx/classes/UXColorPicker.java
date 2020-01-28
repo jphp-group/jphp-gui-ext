91ccb8f5b94779492cfec5d90e685e4c6ef43945
Dmitriy Zayceff <d.zayceff@gmail.com>
2020-01-28 22:04:01 +0300

    Fix colorPicker.value bugs.

--- jphp-gui-ext/src-jvm/main/java/org/develnext/jphp/ext/javafx/classes/UXColorPicker.java
+++ jphp-gui-ext/src-jvm/main/java/org/develnext/jphp/ext/javafx/classes/UXColorPicker.java
@@ -4,18 +4,20 @@ import javafx.collections.ObservableList;
 import javafx.scene.control.ColorPicker;
 import javafx.scene.paint.Color;
 import org.develnext.jphp.ext.javafx.JavaFXExtension;
+import org.develnext.jphp.ext.javafx.classes.paint.UXColor;
+import php.runtime.Memory;
 import php.runtime.annotation.Reflection;
 import php.runtime.annotation.Reflection.Name;
 import php.runtime.annotation.Reflection.Property;
 import php.runtime.annotation.Reflection.Signature;
 import php.runtime.env.Environment;
+import php.runtime.memory.support.MemoryOperation;
 import php.runtime.reflection.ClassEntity;
 
 @Name(JavaFXExtension.NS + "UXColorPicker")
 public class UXColorPicker extends UXComboBoxBase {
     interface WrappedInterface {
         @Property ObservableList<Color> customColors();
-        @Property Color value();
     }
 
     public UXColorPicker(Environment env, ColorPicker wrappedObject) {
@@ -39,5 +41,19 @@ public class UXColorPicker extends UXComboBoxBase {
     @Signature
     public void __construct(Color color) {
         __wrappedObject = new ColorPicker(color);
+    }
+
+
+    @Reflection.Getter
+    protected Memory getValue(Environment env) {
+        return Memory.wrap(env, getWrappedObject().getValue());
+    }
+
+    @Reflection.Setter
+    protected void setValue(Environment env, Memory value) {
+        UXColor color = (UXColor) MemoryOperation.get(UXColor.class, null)
+                .convertNoThrow(env, env.trace(), value);
+
+        getWrappedObject().setValue(color == null ? null : color.getWrappedObject());
     }
 }
